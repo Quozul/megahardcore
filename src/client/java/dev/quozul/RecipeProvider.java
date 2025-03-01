97e209f4788c4ec287acc83d55c081472b1e035b
Quozul <30729291+Quozul@users.noreply.github.com>
2025-03-01 10:57:31 +0100

    fix: recipes

--- src/client/java/dev/quozul/RecipeProvider.java
+++ src/client/java/dev/quozul/RecipeProvider.java
@@ -2,23 +2,24 @@ package dev.quozul;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
-import net.minecraft.data.recipe.CookingRecipeJsonBuilder;
-import net.minecraft.data.recipe.RecipeExporter;
-import net.minecraft.data.recipe.RecipeGenerator;
-import net.minecraft.data.recipe.ShapedRecipeJsonBuilder;
+import net.minecraft.data.recipe.*;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.Items;
-import net.minecraft.recipe.Ingredient;
-import net.minecraft.recipe.RecipeSerializer;
-import net.minecraft.recipe.SmokingRecipe;
+import net.minecraft.recipe.*;
 import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.registry.tag.ItemTags;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
+import static dev.quozul.MegaHardCore.MOD_ID;
+
 public class RecipeProvider extends FabricRecipeProvider {
+
     public RecipeProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
         super(output, registriesFuture);
     }
@@ -48,19 +49,19 @@ public class RecipeProvider extends FabricRecipeProvider {
 
         private void planks() {
             // Overworld logs
-            offerPlanksRecipe(Items.OAK_PLANKS, ItemTags.OAK_LOGS, 3);
-            offerPlanksRecipe(Items.SPRUCE_PLANKS, ItemTags.SPRUCE_LOGS, 3);
-            offerPlanksRecipe(Items.BIRCH_PLANKS, ItemTags.BIRCH_LOGS, 3);
-            offerPlanksRecipe(Items.JUNGLE_PLANKS, ItemTags.JUNGLE_LOGS, 3);
-            offerPlanksRecipe(Items.ACACIA_PLANKS, ItemTags.ACACIA_LOGS, 3);
-            offerPlanksRecipe(Items.CHERRY_PLANKS, ItemTags.CHERRY_LOGS, 3);
-            offerPlanksRecipe(Items.PALE_OAK_PLANKS, ItemTags.PALE_OAK_LOGS, 3);
-            offerPlanksRecipe(Items.DARK_OAK_PLANKS, ItemTags.DARK_OAK_LOGS, 3);
-            offerPlanksRecipe(Items.MANGROVE_PLANKS, ItemTags.MANGROVE_LOGS, 3);
+            offerPlanksRecipe3(Items.OAK_PLANKS, ItemTags.OAK_LOGS);
+            offerPlanksRecipe3(Items.SPRUCE_PLANKS, ItemTags.SPRUCE_LOGS);
+            offerPlanksRecipe3(Items.BIRCH_PLANKS, ItemTags.BIRCH_LOGS);
+            offerPlanksRecipe3(Items.JUNGLE_PLANKS, ItemTags.JUNGLE_LOGS);
+            offerPlanksRecipe3(Items.ACACIA_PLANKS, ItemTags.ACACIA_LOGS);
+            offerPlanksRecipe3(Items.CHERRY_PLANKS, ItemTags.CHERRY_LOGS);
+            offerPlanksRecipe3(Items.PALE_OAK_PLANKS, ItemTags.PALE_OAK_LOGS);
+            offerPlanksRecipe3(Items.DARK_OAK_PLANKS, ItemTags.DARK_OAK_LOGS);
+            offerPlanksRecipe3(Items.MANGROVE_PLANKS, ItemTags.MANGROVE_LOGS);
 
             // Nether logs
-            offerPlanksRecipe(Items.CRIMSON_PLANKS, ItemTags.CRIMSON_STEMS, 3);
-            offerPlanksRecipe(Items.WARPED_PLANKS, ItemTags.WARPED_STEMS, 3);
+            offerPlanksRecipe3(Items.CRIMSON_PLANKS, ItemTags.CRIMSON_STEMS);
+            offerPlanksRecipe3(Items.WARPED_PLANKS, ItemTags.WARPED_STEMS);
         }
 
         private void stoneTools() {
@@ -97,67 +98,98 @@ public class RecipeProvider extends FabricRecipeProvider {
 
         private void ores() {
             // Iron and gold only gives nuggets in regular furnaces
-            offerSmelting(List.of(Items.DEEPSLATE_IRON_ORE), RecipeCategory.MISC, Items.IRON_NUGGET, 0.7f, 200, "iron_nugget");
-            offerSmelting(List.of(Items.IRON_ORE), RecipeCategory.MISC, Items.IRON_NUGGET, 0.7f, 200, "iron_nugget");
-            offerSmelting(List.of(Items.RAW_IRON), RecipeCategory.MISC, Items.IRON_NUGGET, 0.7f, 200, "iron_nugget");
-
-            offerSmelting(List.of(Items.DEEPSLATE_GOLD_ORE), RecipeCategory.MISC, Items.GOLD_NUGGET, 1f, 200, "gold_nugget");
-            offerSmelting(List.of(Items.GOLD_ORE), RecipeCategory.MISC, Items.GOLD_NUGGET, 1f, 200, "gold_nugget");
-            offerSmelting(List.of(Items.NETHER_GOLD_ORE), RecipeCategory.MISC, Items.GOLD_NUGGET, 1f, 200, "gold_nugget");
-            offerSmelting(List.of(Items.RAW_GOLD), RecipeCategory.MISC, Items.GOLD_NUGGET, 1f, 200, "gold_nugget");
+            offerSmelting2(List.of(Items.DEEPSLATE_IRON_ORE), Items.IRON_NUGGET, 0.7f, "iron_nugget");
+            offerSmelting2(List.of(Items.IRON_ORE), Items.IRON_NUGGET, 0.7f, "iron_nugget");
+            offerSmelting2(List.of(Items.RAW_IRON), Items.IRON_NUGGET, 0.7f, "iron_nugget");
+            offerSmelting2(List.of(Items.DEEPSLATE_GOLD_ORE), Items.GOLD_NUGGET, 1f, "gold_nugget");
+            offerSmelting2(List.of(Items.GOLD_ORE), Items.GOLD_NUGGET, 1f, "gold_nugget");
+            offerSmelting2(List.of(Items.NETHER_GOLD_ORE), Items.GOLD_NUGGET, 1f, "gold_nugget");
+            offerSmelting2(List.of(Items.RAW_GOLD), Items.GOLD_NUGGET, 1f, "gold_nugget");
 
             // Those are the vanilla blasting recipes, the only difference is the cooking time which has been increased
             // from 100 to 200 which is the same as all those recipes in a regular furnace
-            offerBlasting(List.of(Items.COAL_ORE), RecipeCategory.MISC, Items.COAL, 0.1f, 200, "coal");
-            offerBlasting(List.of(Items.DEEPSLATE_COAL_ORE), RecipeCategory.MISC, Items.COAL, 0.1f, 200, "coal");
-            offerBlasting(List.of(Items.COPPER_ORE), RecipeCategory.MISC, Items.COPPER_INGOT, 0.7f, 200, "copper_ingot");
-            offerBlasting(List.of(Items.DEEPSLATE_COPPER_ORE), RecipeCategory.MISC, Items.COPPER_INGOT, 0.7f, 200, "copper_ingot");
-            offerBlasting(List.of(Items.RAW_COPPER), RecipeCategory.MISC, Items.COPPER_INGOT, 0.7f, 200, "copper_ingot");
-            offerBlasting(List.of(Items.DEEPSLATE_DIAMOND_ORE), RecipeCategory.MISC, Items.DIAMOND, 1f, 200, "diamond");
-            offerBlasting(List.of(Items.DIAMOND_ORE), RecipeCategory.MISC, Items.DIAMOND, 1f, 200, "diamond");
-            offerBlasting(List.of(Items.DEEPSLATE_EMERALD_ORE), RecipeCategory.MISC, Items.EMERALD, 1f, 200, "emerald");
-            offerBlasting(List.of(Items.EMERALD_ORE), RecipeCategory.MISC, Items.EMERALD, 1f, 200, "emerald");
-            offerBlasting(List.of(Items.DEEPSLATE_GOLD_ORE), RecipeCategory.MISC, Items.GOLD_INGOT, 1f, 200, "gold_ingot");
-            offerBlasting(List.of(Items.GOLD_ORE), RecipeCategory.MISC, Items.GOLD_INGOT, 1f, 200, "gold_ingot");
-            offerBlasting(List.of(Items.NETHER_GOLD_ORE), RecipeCategory.MISC, Items.GOLD_INGOT, 1f, 200, "gold_ingot");
-            offerBlasting(List.of(Items.RAW_GOLD), RecipeCategory.MISC, Items.GOLD_INGOT, 1f, 200, "gold_ingot");
-            offerBlasting(List.of(Items.GOLDEN_PICKAXE, Items.GOLDEN_SHOVEL, Items.GOLDEN_AXE, Items.GOLDEN_HOE, Items.GOLDEN_SWORD, Items.GOLDEN_HELMET, Items.GOLDEN_CHESTPLATE, Items.GOLDEN_LEGGINGS, Items.GOLDEN_BOOTS, Items.GOLDEN_HORSE_ARMOR), RecipeCategory.MISC, Items.GOLD_NUGGET, 0.1f, 200, "gold_nugget");
-            offerBlasting(List.of(Items.DEEPSLATE_IRON_ORE), RecipeCategory.MISC, Items.IRON_INGOT, 0.7f, 200, "iron_ingot");
-            offerBlasting(List.of(Items.IRON_ORE), RecipeCategory.MISC, Items.IRON_INGOT, 0.7f, 200, "iron_ingot");
-            offerBlasting(List.of(Items.RAW_IRON), RecipeCategory.MISC, Items.IRON_INGOT, 0.7f, 200, "iron_ingot");
-            offerBlasting(List.of(Items.IRON_PICKAXE, Items.IRON_SHOVEL, Items.IRON_AXE, Items.IRON_HOE, Items.IRON_SWORD, Items.IRON_HELMET, Items.IRON_CHESTPLATE, Items.IRON_LEGGINGS, Items.IRON_BOOTS, Items.IRON_HORSE_ARMOR, Items.CHAINMAIL_HELMET, Items.CHAINMAIL_CHESTPLATE, Items.CHAINMAIL_LEGGINGS, Items.CHAINMAIL_BOOTS), RecipeCategory.MISC, Items.IRON_NUGGET, 0.1f, 200, "iron_nugget");
-            offerBlasting(List.of(Items.DEEPSLATE_LAPIS_ORE), RecipeCategory.MISC, Items.LAPIS_LAZULI, 0.2f, 200, "lapis_lazuli");
-            offerBlasting(List.of(Items.LAPIS_ORE), RecipeCategory.MISC, Items.LAPIS_LAZULI, 0.2f, 200, "lapis_lazuli");
-            offerBlasting(List.of(Items.ANCIENT_DEBRIS), RecipeCategory.MISC, Items.NETHERITE_SCRAP, 2f, 200, "netherite_scrap");
-            offerBlasting(List.of(Items.NETHER_QUARTZ_ORE), RecipeCategory.MISC, Items.QUARTZ, 0.2f, 200, "quartz");
-            offerBlasting(List.of(Items.DEEPSLATE_REDSTONE_ORE), RecipeCategory.MISC, Items.REDSTONE, 0.7f, 200, "redstone");
-            offerBlasting(List.of(Items.REDSTONE_ORE), RecipeCategory.MISC, Items.REDSTONE, 0.7f, 200, "redstone");
+            offerBlasting2(List.of(Items.COAL_ORE), Items.COAL, 0.1f, "coal");
+            offerBlasting2(List.of(Items.DEEPSLATE_COAL_ORE), Items.COAL, 0.1f, "coal");
+            offerBlasting2(List.of(Items.COPPER_ORE), Items.COPPER_INGOT, 0.7f, "copper_ingot");
+            offerBlasting2(List.of(Items.DEEPSLATE_COPPER_ORE), Items.COPPER_INGOT, 0.7f, "copper_ingot");
+            offerBlasting2(List.of(Items.RAW_COPPER), Items.COPPER_INGOT, 0.7f, "copper_ingot");
+            offerBlasting2(List.of(Items.DEEPSLATE_DIAMOND_ORE), Items.DIAMOND, 1f, "diamond");
+            offerBlasting2(List.of(Items.DIAMOND_ORE), Items.DIAMOND, 1f, "diamond");
+            offerBlasting2(List.of(Items.DEEPSLATE_EMERALD_ORE), Items.EMERALD, 1f, "emerald");
+            offerBlasting2(List.of(Items.EMERALD_ORE), Items.EMERALD, 1f, "emerald");
+            offerBlasting2(List.of(Items.DEEPSLATE_GOLD_ORE), Items.GOLD_INGOT, 1f, "gold_ingot");
+            offerBlasting2(List.of(Items.GOLD_ORE), Items.GOLD_INGOT, 1f, "gold_ingot");
+            offerBlasting2(List.of(Items.NETHER_GOLD_ORE), Items.GOLD_INGOT, 1f, "gold_ingot");
+            offerBlasting2(List.of(Items.RAW_GOLD), Items.GOLD_INGOT, 1f, "gold_ingot");
+            offerBlasting2(List.of(Items.DEEPSLATE_IRON_ORE), Items.IRON_INGOT, 0.7f, "iron_ingot");
+            offerBlasting2(List.of(Items.IRON_ORE), Items.IRON_INGOT, 0.7f, "iron_ingot");
+            offerBlasting2(List.of(Items.RAW_IRON), Items.IRON_INGOT, 0.7f, "iron_ingot");
+            offerBlasting2(List.of(Items.DEEPSLATE_LAPIS_ORE), Items.LAPIS_LAZULI, 0.2f, "lapis_lazuli");
+            offerBlasting2(List.of(Items.LAPIS_ORE), Items.LAPIS_LAZULI, 0.2f, "lapis_lazuli");
+            offerBlasting2(List.of(Items.ANCIENT_DEBRIS), Items.NETHERITE_SCRAP, 2f, "netherite_scrap");
+            offerBlasting2(List.of(Items.NETHER_QUARTZ_ORE), Items.QUARTZ, 0.2f, "quartz");
+            offerBlasting2(List.of(Items.DEEPSLATE_REDSTONE_ORE), Items.REDSTONE, 0.7f, "redstone");
+            offerBlasting2(List.of(Items.REDSTONE_ORE), Items.REDSTONE, 0.7f, "redstone");
+            offerBlasting2(List.of(Items.GOLDEN_PICKAXE, Items.GOLDEN_SHOVEL, Items.GOLDEN_AXE, Items.GOLDEN_HOE, Items.GOLDEN_SWORD, Items.GOLDEN_HELMET, Items.GOLDEN_CHESTPLATE, Items.GOLDEN_LEGGINGS, Items.GOLDEN_BOOTS, Items.GOLDEN_HORSE_ARMOR), Items.GOLD_NUGGET, 0.1f, "gold_nugget");
+            offerBlasting2(List.of(Items.IRON_PICKAXE, Items.IRON_SHOVEL, Items.IRON_AXE, Items.IRON_HOE, Items.IRON_SWORD, Items.IRON_HELMET, Items.IRON_CHESTPLATE, Items.IRON_LEGGINGS, Items.IRON_BOOTS, Items.IRON_HORSE_ARMOR, Items.CHAINMAIL_HELMET, Items.CHAINMAIL_CHESTPLATE, Items.CHAINMAIL_LEGGINGS, Items.CHAINMAIL_BOOTS), Items.IRON_NUGGET, 0.1f, "iron_nugget");
         }
 
         private void food() {
-            offerSmoking(Items.POTATO, RecipeCategory.MISC, Items.BAKED_POTATO, 0.35f, 200, "baked_potato");
-            offerSmoking(Items.BEEF, RecipeCategory.MISC, Items.COOKED_BEEF, 0.35f, 200, "cooked_beef");
-            offerSmoking(Items.CHICKEN, RecipeCategory.MISC, Items.COOKED_CHICKEN, 0.35f, 200, "cooked_chicken");
-            offerSmoking(Items.COD, RecipeCategory.MISC, Items.COOKED_COD, 0.35f, 200, "cooked_cod");
-            offerSmoking(Items.MUTTON, RecipeCategory.MISC, Items.COOKED_MUTTON, 0.35f, 200, "cooked_mutton");
-            offerSmoking(Items.PORKCHOP, RecipeCategory.MISC, Items.COOKED_PORKCHOP, 0.35f, 200, "cooked_porkchop");
-            offerSmoking(Items.RABBIT, RecipeCategory.MISC, Items.COOKED_RABBIT, 0.35f, 200, "cooked_rabbit");
-            offerSmoking(Items.SALMON, RecipeCategory.MISC, Items.COOKED_SALMON, 0.35f, 200, "cooked_salmon");
-            offerSmoking(Items.KELP, RecipeCategory.MISC, Items.DRIED_KELP, 0.1f, 200, "dried_kelp");
+            offerSmoking(Items.POTATO, Items.BAKED_POTATO, 0.35f, "baked_potato");
+            offerSmoking(Items.BEEF, Items.COOKED_BEEF, 0.35f, "cooked_beef");
+            offerSmoking(Items.CHICKEN, Items.COOKED_CHICKEN, 0.35f, "cooked_chicken");
+            offerSmoking(Items.COD, Items.COOKED_COD, 0.35f, "cooked_cod");
+            offerSmoking(Items.MUTTON, Items.COOKED_MUTTON, 0.35f, "cooked_mutton");
+            offerSmoking(Items.PORKCHOP, Items.COOKED_PORKCHOP, 0.35f, "cooked_porkchop");
+            offerSmoking(Items.RABBIT, Items.COOKED_RABBIT, 0.35f, "cooked_rabbit");
+            offerSmoking(Items.SALMON, Items.COOKED_SALMON, 0.35f, "cooked_salmon");
+            offerSmoking(Items.KELP, Items.DRIED_KELP, 0.1f, "dried_kelp");
         }
 
-        private void offerSmoking(ItemConvertible itemConvertible, RecipeCategory category, ItemConvertible output, float experience, int cookingTime, String group) {
-            CookingRecipeJsonBuilder.create(Ingredient.ofItem(itemConvertible), category, output, experience, cookingTime, RecipeSerializer.SMOKING, SmokingRecipe::new)
+        private void offerSmelting2(List<ItemConvertible> itemConvertibles, ItemConvertible output, float experience, String group) {
+            for (ItemConvertible itemConvertible : itemConvertibles) {
+                CookingRecipeJsonBuilder.create(Ingredient.ofItem(itemConvertible), RecipeCategory.MISC, output, experience, 200, RecipeSerializer.SMELTING, SmeltingRecipe::new)
+                        .group(group)
+                        .criterion(hasItem(itemConvertible), this.conditionsFromItem(itemConvertible))
+                        .offerTo(this.exporter, getPath(output) + "_from_smelting_" + getItemPath(itemConvertible));
+            }
+        }
+
+        private void offerBlasting2(List<ItemConvertible> itemConvertibles, ItemConvertible output, float experience, String group) {
+            for (ItemConvertible itemConvertible : itemConvertibles) {
+                CookingRecipeJsonBuilder.create(Ingredient.ofItem(itemConvertible), RecipeCategory.MISC, output, experience, 200, RecipeSerializer.BLASTING, BlastingRecipe::new)
+                        .group(group)
+                        .criterion(hasItem(itemConvertible), this.conditionsFromItem(itemConvertible))
+                        .offerTo(this.exporter, getPath(output) + "_from_blasting_" + getItemPath(itemConvertible));
+            }
+        }
+
+        private void offerSmoking(ItemConvertible itemConvertible, ItemConvertible output, float experience, String group) {
+            CookingRecipeJsonBuilder.create(Ingredient.ofItem(itemConvertible), RecipeCategory.FOOD, output, experience, 200, RecipeSerializer.SMOKING, SmokingRecipe::new)
                     .group(group)
                     .criterion(hasItem(itemConvertible), this.conditionsFromItem(itemConvertible))
-                    .offerTo(this.exporter, getItemPath(output) + "_from_smoking");
+                    .offerTo(this.exporter, getPath(output) + "_from_smoking");
         }
 
         private void finishToolBuilder(ShapedRecipeJsonBuilder builder) {
             builder.input('s', Items.STICK)
                     .input('f', Items.FLINT)
                     .criterion(hasItem(Items.FLINT), conditionsFromItem(Items.FLINT))
-                    .offerTo(exporter);
+                    .offerTo(exporter, getPath(builder.getOutputItem()));
+        }
+
+        private void offerPlanksRecipe3(ItemConvertible output, TagKey<Item> logTag) {
+            Identifier itemId = CraftingRecipeJsonBuilder.getItemId(output);
+            this.createShapeless(RecipeCategory.BUILDING_BLOCKS, output, 3)
+                    .input(logTag)
+                    .group("planks")
+                    .criterion("has_logs", this.conditionsFromTag(logTag))
+                    .offerTo(exporter, getPath(output));
+        }
+
+        private String getPath(ItemConvertible output) {
+            Identifier itemId = CraftingRecipeJsonBuilder.getItemId(output);
+            return Identifier.of(MOD_ID, itemId.getPath()).toString();
         }
     }
 }
--- src/main/java/dev/quozul/mixin/ServerRecipeManagerMixin.java
+++ src/main/java/dev/quozul/mixin/ServerRecipeManagerMixin.java
@@ -1,5 +1,6 @@
 package dev.quozul.mixin;
 
+import dev.quozul.MegaHardCore;
 import net.minecraft.recipe.*;
 import net.minecraft.recipe.book.RecipeBookCategories;
 import net.minecraft.resource.ResourceManager;
@@ -7,6 +8,7 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.profiler.Profiler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -14,43 +16,71 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static dev.quozul.MegaHardCore.MOD_ID;
+
 @Mixin(ServerRecipeManager.class)
 public class ServerRecipeManagerMixin {
-
     @Shadow
     private PreparedRecipes preparedRecipes;
 
-    /**
-     * Disables copper ingot and food in regular furnaces.
-     * Disables Netherite upgrade duplication craft.
-     */
     @Inject(method = "apply(Lnet/minecraft/recipe/PreparedRecipes;Lnet/minecraft/resource/ResourceManager;Lnet/minecraft/util/profiler/Profiler;)V", at = @At("RETURN"))
     private void onApply(PreparedRecipes preparedRecipes, ResourceManager resourceManager, Profiler profiler, CallbackInfo ci) {
         List<Identifier> recipesToRemove = List.of(
                 Identifier.of("minecraft", "netherite_upgrade_smithing_template"),
-                Identifier.of("minecraft", "stone_axe"),
-                Identifier.of("minecraft", "stone_hoe"),
-                Identifier.of("minecraft", "stone_pickaxe"),
-                Identifier.of("minecraft", "stone_pickaxe"),
-                Identifier.of("minecraft", "stone_shovel"),
-                Identifier.of("minecraft", "stone_sword")
+                Identifier.of("minecraft", "iron_ingot_from_smelting_iron_ore"),
+                Identifier.of("minecraft", "iron_ingot_from_smelting_raw_iron"),
+                Identifier.of("minecraft", "iron_ingot_from_smelting_deepslate_iron_ore"),
+                Identifier.of("minecraft", "gold_ingot_from_smelting_iron_ore"),
+                Identifier.of("minecraft", "gold_ingot_from_smelting_raw_gold"),
+                Identifier.of("minecraft", "gold_ingot_from_smelting_deepslate_gold_ore")
         );
+
+        // A list of all mod's recipes paths to remove duplicated recipes
+        List<String> replacedRecipes = this.preparedRecipes.recipes().stream().filter(entry -> {
+            Identifier identifier = entry.id().getValue();
+            return identifier.getNamespace().equals(MOD_ID);
+        }).map(entry -> entry.id().getValue().getPath()).toList();
 
         List<RecipeEntry<?>> filtered = this.preparedRecipes.recipes().stream()
                 .filter(entry -> {
-                    Recipe<?> recipe = entry.value();
-                    if (recipe.getType() == RecipeType.SMELTING) {
-                        SmeltingRecipe smeltingRecipe = (SmeltingRecipe) recipe;
-                        if (smeltingRecipe.getGroup().equals("copper_ingot") ||
-                                smeltingRecipe.getRecipeBookCategory().equals(RecipeBookCategories.FURNACE_FOOD)
-                        ) {
-                            return false;
-                        }
+                    boolean removeRecipe = shouldRemoveRecipe(entry, recipesToRemove, replacedRecipes);
+                    if (removeRecipe) {
+                        MegaHardCore.LOGGER.info("Removing recipe {}", entry.id().getValue().toString());
                     }
-                    return !recipesToRemove.contains(entry.id().getValue());
+                    return !removeRecipe;
                 })
                 .collect(Collectors.toList());
 
         this.preparedRecipes = PreparedRecipes.of(filtered);
+    }
+
+    @Unique
+    private static boolean shouldRemoveCraftingRecipe(RecipeEntry<?> entry, List<Identifier> recipesToRemove, List<String> replacedRecipes) {
+        Identifier identifier = entry.id().getValue();
+        String namespace = identifier.getNamespace();
+        String path = identifier.getPath();
+        boolean isModRecipe = namespace.equals(MOD_ID);
+        boolean isRecipeToRemove = recipesToRemove.contains(identifier);
+        boolean isDuplicateRecipe = !isModRecipe && replacedRecipes.contains(path);
+        return isDuplicateRecipe || isRecipeToRemove;
+    }
+
+    @Unique
+    private static boolean shouldRemoveSmeltingRecipe(SmeltingRecipe smeltingRecipe) {
+        boolean isOreGroup = smeltingRecipe.getGroup().equals("copper_ingot") || smeltingRecipe.getGroup().equals("gold_ingot") || smeltingRecipe.getGroup().equals("iron_ingot");
+        boolean isFoodCategory = smeltingRecipe.getRecipeBookCategory().equals(RecipeBookCategories.FURNACE_FOOD);
+        return isOreGroup || isFoodCategory;
+    }
+
+    @Unique
+    private static boolean shouldRemoveRecipe(RecipeEntry<?> entry, List<Identifier> recipesToRemove, List<String> replacedRecipes) {
+        Recipe<?> recipe = entry.value();
+        if (recipe.getType() == RecipeType.SMELTING) {
+            SmeltingRecipe smeltingRecipe = (SmeltingRecipe) recipe;
+            if (shouldRemoveSmeltingRecipe(smeltingRecipe)) {
+                return true;
+            }
+        }
+        return shouldRemoveCraftingRecipe(entry, recipesToRemove, replacedRecipes);
     }
 }
